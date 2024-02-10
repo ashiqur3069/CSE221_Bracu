40ee56a3fea572c8ba66f6b2bee029df0e4fd0d8
Syed Ashiqur Rahman <112290585+ashiqur3069@users.noreply.github.com>
2024-02-11 02:43:42 +0600

    Add files via upload

---
+++ Assignment 02/Problem_2/Task2_O(n).py
@@ -0,0 +1,23 @@
+def marge_list(size1,size2,list1,list2):
+    array = []
+    i, j = 0, 0
+    while i < int(size1)-1 and j < int(size2)-1:
+        if int(list1[i]) < int(list2[j]):
+            array.append(list1[i])
+            i += 1
+        else:
+            array.append(list2[j])
+            j+= 1
+    array += list1[i:]
+    array += list2[j:]
+    return array
+
+with open("input2.txt","r") as file:
+    size1 = file.readline()
+    list1 = file.readline().strip().split()
+    size2 = file.readline()
+    list2 = file.readline().strip().split()
+    output = marge_list(size1,size2,list1,list2)
+    with open("output1.txt", "w") as file2:
+        for i in output:
+            file2.write(f"{i} ")
---
+++ Assignment 02/Problem_2/Task2_O(nlogn).py
@@ -0,0 +1,20 @@
+
+def marge_list(array):
+    return sorted(array)
+with open("input2.txt","r") as file:
+    size1 = file.readline()
+    list1 = file.readline().strip().split()
+    size2 = file.readline()
+    list2 = file.readline().strip().split()
+
+    array = []
+    for i in list1:
+        array.append(int(i))
+    for j in list2:
+        array.append(int(j))
+    output = marge_list(array)
+
+    with open("output1.txt", "w") as file2:
+
+        for i in output:
+            file2.write(f"{i} ")
